0789cf2326d36d4092deb073be162bb663cda50e
Luca Braglia <lbraglia@gmail.com>
2016-01-19 16:44:42 +0100

    fix tests

--- tests/testthat/test-data-management.R
+++ tests/testthat/test-data-management.R
@@ -3,14 +3,14 @@ context("Data management")
 test_that("duplicated2 flag all the duplicates by default", {
 
     expect_identical(duplicated2(c(0, 1, 1, 2, 2, NA, NA)),
-                     c(FALSE, rep(TRUE, 6))
+                     c(FALSE, rep(TRUE, 6)))
 
 })
 
 
 test_that("is.unique works correctly", {
 
-    expect_identical(is.unique(1:3), TRUE)
-    expect_identical(is.unique(c('a','a','b'), FALSE)
+    expect_true(is.unique(1:3))
+    expect_false(is.unique(c('a', 'a', 'b')))
 
 })
