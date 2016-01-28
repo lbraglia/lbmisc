935c362d6e2f39257edf5ce233f2c7bcefed0116
Luca Braglia <lbraglia@gmail.com>
2016-01-28 09:40:42 +0100

    update

---
+++ R/misc_utils.R
@@ -0,0 +1,19 @@
+#' update package shortcut
+#'
+#' update package shortcut with my defaults
+#' @param ask same as \code{\link{update.packages}}
+#' @export 
+update_packages <- function(ask = FALSE) {
+
+  personal_library <- grep(path.expand("~/R"), .libPaths(), value = TRUE)
+  update.packages(lib.loc = personal_library ,
+                  repos = "http://cran.rstudio.com",
+                  ask = ask)
+}
+
+#' read R news
+#'
+#' read R news
+#' @param ... arguments passed to \code{\link{news}}
+#' @export
+rnews <- function(...) utils::page(utils::news(...), method = 'print')
