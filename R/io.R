8b4f49b8769d4aca0bec0cbe3d5785bca38275fa
Luca Braglia <lbraglia@gmail.com>
2016-06-13 10:51:37 +0200

    update

--- R/io.R
+++ R/io.R
@@ -3,7 +3,7 @@
 #' A function to export an openxlsx workbook oin xls format quickly
 #' @param wb openxlsx workbook
 #' @param destfile xls destination file
-#' 
+#' @export
 wb_to_xls <- function(wb, destfile = NULL){
     xlsx_file <- paste0(tools::file_path_sans_ext(destfile), '.xlsx')
     on.exit(unlink(xlsx_file))
