e2ddfb9ca5f3f9867d44912b31b42192723d9a00
Luca Braglia <lbraglia@gmail.com>
2017-11-14 17:07:45 +0100

    update

---
+++ R/inches.R
@@ -0,0 +1,12 @@
+#' Translates from cm (centimeters) to inches.
+#' 
+#' 
+#' Translates from cm (centimeters) to inches.
+#' 
+#' 
+#' @param cm Numeric vector of centimeter.
+#' @return A vector with inches.
+#' @export
+inches <- function(cm) {
+    cm * 0.393700787
+}
