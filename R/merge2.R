05294cc75c554cdb84b20ce979ffb653ca657e93
Luca Braglia <lbraglia@gmail.com>
2020-01-21 10:13:23 +0100

    update

--- R/merge2.R
+++ R/merge2.R
@@ -42,23 +42,49 @@ merge2.data.frame <- function(x, y,
                               incomparables = NULL, ...)
 {
 
-    ## ----------------------
-    ## handle duplicated IDS
-    ## ----------------------
-    dupl_x <- x[duplicated(x[, by.x]), by.x, drop = FALSE]
-    dupl_y <- y[duplicated(y[, by.y]), by.y, drop = FALSE]
+    x_ids <- x[, by.x, drop = FALSE]
+    y_ids <- y[, by.y, drop = FALSE]
+    
+    ## --------------------------
+    ## warning for duplicated IDS
+    ## --------------------------
+    dupl_x <- x_ids[duplicated(x_ids), , drop = FALSE]
+    dupl_y <- y_ids[duplicated(y_ids), , drop = FALSE]
 
+    rownames(dupl_x) <- NULL
+    rownames(dupl_y) <- NULL
+    
     dupl_ids <- function(z, who){
         if (nrow(z) > 0L){
-            warning('Duplicates ID in ', who, '; unique IDs are\n')
+            cat("\n=============================================\n")
+            warning('Duplicates ids in ', who)
+            cat("=============================================\n")
             print(unique(z))
-            cat("\n")
+            cat("=========================================\n")
         } else invisible(NULL)
     }
 
     dupl_ids(dupl_x, 'x')
     dupl_ids(dupl_y, 'y')
 
+    ## ----------------------------------------------
+    ## message for IDS not found in the other dataset
+    ## ----------------------------------------------
+    a_rows_not_in_b <- function(a, b, aname, bname) {
+        res <- a[do.call(paste0, a) %nin% do.call(paste0, b), , drop = FALSE]
+        if (nrow(res) > 0L){
+            cat("\n=========================================\n")
+            message(aname, "'s ids not available in ", bname)
+            cat("=========================================\n")
+            print(res)
+            cat("=========================================\n")
+        }
+    }
+   
+    ## x_ids %nin% y_ids
+    a_rows_not_in_b(x_ids, y_ids, 'x', 'y')
+    a_rows_not_in_b(y_ids, x_ids, 'y', 'x')
+    
     ## ----------------------
     ## standard merge
     ## ----------------------
@@ -78,5 +104,3 @@ merge2.data.frame <- function(x, y,
                      ...)
                      
 }
-    
-
