6888ca136e173f1486074decf2acb252a63729b8
Luca Braglia <lbraglia@gmail.com>
2022-04-01 13:48:14 +0200

    update

---
+++ R/install_scripts.R
@@ -0,0 +1,63 @@
+#' Install scripts from a package creating symlinks
+#'
+#' @param package name of the package
+#' @param into destination directory of the created symlinks
+#' @param location where to search the installed package is
+#' @source code from
+#'     \url{https://stackoverflow.com/questions/44566100}
+#' @examples
+#' ## lbmisc::install_scripts(package = 'speeches')
+#' @export
+install_scripts <- function(package = NULL,
+                            into = "~/.local/bin",
+                            location = .libPaths()[1],
+                            overwrite = FALSE)
+{
+    # paths
+    if (is.null(package))
+        stop("Specify the package")
+    from_path <- file.path(location, package)
+    if (!dir.exists(from_path))
+        stop(package, " is not installed in ", location, ". Aborting")
+    exec_path <- file.path(from_path, 'exec')
+    scripts <- dir(exec_path, full.names = TRUE)
+    if (length(scripts) == 0) {
+        # exit but do not raise anything (in order not to stop pake/make)
+        message("No scripts to be linked in ", exec_path, " . Bypassing")
+        invisible(NULL)
+    }
+    if (!dir.exists(into)) dir.create(into)
+    into <- normalizePath(into)
+    dests <- file.path(normalizePath(into), basename(scripts))
+
+    # overwrite handling
+    if (any(already_exist <- file.exists(dests))) {
+        if (overwrite) {
+            to_del <- dests[already_exist]
+            cat("Deleting existing file: ", to_del)
+            unlink(to_del)
+        } else {
+            msg <- sprintf("Skipping '%s': a file by that name already exists",
+                           basename(scripts[already_exist]))
+            cat(msg)
+            scripts <- scripts[!already_exist]
+            dests   <-   dests[!already_exist]
+        }
+    }
+
+    # link creation
+    if (length(scripts)) {
+        file.symlink(scripts, dests)
+        cat("Created symlinks:")
+        cat(dests, " ->\n    ", scripts, bullet_col = "green")
+    } else
+        cat("Nothing installed")
+
+    ## path check
+    PATHS <- normalizePath(
+        strsplit(Sys.getenv("PATH"), ":", fixed = TRUE)[[1]],
+                           mustWork = FALSE)
+    if(!into %in% PATHS)
+        warning(sprintf("destination '%s' is not on the PATH", into))
+    
+}
